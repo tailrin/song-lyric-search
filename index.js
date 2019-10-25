d071097e8710efd72541ce2ae720d439267987fc
josh <tailrin.jh@gmail.com>
2019-10-25 10:37:15 -0600

    added files

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 
 function getLyrics(artist, title) {
   const url = `https://api.lyrics.ovh/v1/${encodeURI(artist)}/${encodeURI(title)}`;
-  console.log(url);
   fetch(url).then(response => response.json()).then(responseJson => {
     displayResults(responseJson);
   }).catch(err => console.log(err));
 }
 
 function displayResults(responseJson) {
-  $('#results').html(responseJson.lyrics.replace(/\n/g, "<br>"));
   $('#results').removeClass('hidden');
+  $('#results').html(responseJson.lyrics.replace(/\n/g, "<br>"));
 }
 
 function watchForm() {
